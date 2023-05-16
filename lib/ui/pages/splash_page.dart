742fb51fb02de0b256a4f6d0750918a7ccf425ab
Rum <muhammadrum135@gmail.com>
2023-05-16 23:24:12 +0800

    Penyesuaian splash screen

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:kamus_bugis/cubit/check_compare_word_local_cubit.dart';
 import 'package:kamus_bugis/cubit/check_list_word_local_cubit.dart';
@@ -35,15 +37,10 @@ class _SplashPageState extends State<SplashPage> {
     bool listWordBugisDataLocal = false;
     bool listWordIndoDataLocal = false;
 
-    bool checkDataLocal() {
-      if (sentenceDataLocal == true &&
-          compareWordDataLocal == true &&
-          listWordBugisDataLocal == true &&
-          listWordIndoDataLocal == true) {
-        return true;
-      } else {
-        return false;
-      }
+    void checkDataLocal(bool status) {
+      log(status.toString());
+
+      compareWordDataLocal = status;
     }
 
     return Scaffold(
@@ -63,191 +60,50 @@ class _SplashPageState extends State<SplashPage> {
                   const SizedBox(
                     height: 42,
                   ),
-                  BlocBuilder<CheckListWordLocalCubit, CheckListWordLocalState>(
-                    builder: (context, state) {
-                      if (state is CheckListWordIndoLocalSuccess) {
-                        if (state.status == "Data Exist") {
-                          listWordIndoDataLocal = true;
-                          return Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text("Daftar Kata Ada"),
-                              Icon(
-                                Icons.check_circle,
-                                color: Colors.green,
-                              )
-                            ],
-                          );
-                        } else {
-                          listWordIndoDataLocal = false;
-                          context.read<ListWordCubit>().getListWordIndoBugis();
-                          return BlocConsumer<ListWordCubit, ListWordState>(
-                            listener: (context, state) {
-                              if (state is ListWordIndoSuccess) {
-                                context
-                                    .read<CheckListWordLocalCubit>()
-                                    .checkListWordIndoLocal();
-                              }
-                            },
-                            builder: (context, state) {
-                              if (state is ListWordFailed) {
-                                return Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Text("Gagal Get Daftar Kata !"),
-                                    Icon(
-                                      Icons.warning_rounded,
-                                      color: Colors.red,
-                                    )
-                                  ],
-                                );
-                              } else if (state is ListWordLoading) {
-                                return const Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              } else {
-                                return const Center(
-                                  child: Text("Ada kesalahan"),
-                                );
-                              }
-                            },
-                          );
-                        }
-                      } else if (state is CheckListWordLocalLoading) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      } else {
-                        return Center(
-                          child: Text("Ada Kesalahan !"),
-                        );
-                      }
-                    },
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  BlocBuilder<CheckSentenceLocalCubit, CheckSentenceLocalState>(
-                    builder: (context, state) {
-                      if (state is CheckSentenceLocalSuccess) {
-                        if (state.status == "Data Exist") {
-                          sentenceDataLocal = true;
-                          return Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text("Data Kalimat Ada"),
-                              Icon(
-                                Icons.check_circle,
-                                color: Colors.green,
-                              )
-                            ],
-                          );
-                        } else {
-                          sentenceDataLocal = false;
-                          context.read<ListSentenceCubit>().getListSentence();
-                          return BlocConsumer<ListSentenceCubit,
-                              ListSentenceState>(
-                            listener: (context, state) {
-                              if (state is ListSentenceSuccess) {
-                                context
-                                    .read<CheckSentenceLocalCubit>()
-                                    .checkSentenceLocal();
-                              }
-                            },
-                            builder: (context, state) {
-                              if (state is ListSentenceFailed) {
-                                print(state.error);
-                                return Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Text("Gagal get data Kalimat !"),
-                                    Icon(
-                                      Icons.warning_rounded,
-                                      color: Colors.red,
-                                    )
-                                  ],
-                                );
-                              } else if (state is ListSentenceLoading) {
-                                return const Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              } else {
-                                return const Center(
-                                  child: Text("Ada kesalahan"),
-                                );
-                              }
-                            },
-                          );
-                        }
-                      } else if (state is CheckSentenceLocalLoading) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      } else {
-                        return Center(
-                          child: Text("Ada Kesalahan !"),
-                        );
-                      }
-                    },
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
                   BlocBuilder<CheckCompareWordLocalCubit,
                       CheckCompareWordLocalState>(
                     builder: (context, state) {
                       if (state is CheckCompareWordLocalSuccess) {
                         if (state.status == "Data Exist") {
-                          compareWordDataLocal = true;
-                          return Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text("Data Perbandingan Kata Ada"),
-                              Icon(
-                                Icons.check_circle,
-                                color: Colors.green,
-                              )
-                            ],
-                          );
+                          checkDataLocal(false);
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, 'home', (route) => false);
+                          return SizedBox();
                         } else {
-                          compareWordDataLocal = false;
-                          context
-                              .read<ListComparissonCubit>()
-                              .getListComparisson();
-                          return BlocConsumer<ListComparissonCubit,
-                              ListComparissonState>(
-                            listener: (context, state) {
-                              if (state is ListComparissonSuccess) {
-                                context
-                                    .read<CheckCompareWordLocalCubit>()
-                                    .checkCompareWordLocal();
-                              }
-                            },
-                            builder: (context, state) {
-                              if (state is ListComparissonFailed) {
-                                return Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Text("Gagal get data Perbandingan Kata !"),
-                                    Icon(
-                                      Icons.warning_rounded,
-                                      color: Colors.red,
-                                    )
-                                  ],
-                                );
-                              } else if (state is ListComparissonLoading) {
-                                return const Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              } else {
-                                return const Center(
-                                  child: Text("Ada kesalahan"),
-                                );
-                              }
-                            },
+                          checkDataLocal(true);
+                          return Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text("Gagal memuat data !"),
+                                  Icon(
+                                    Icons.warning_rounded,
+                                    color: Colors.red,
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 24,
+                              ),
+                              PrimaryButton(
+                                  title: "Muat ulang",
+                                  onTap: () {
+                                    context
+                                        .read<CheckSentenceLocalCubit>()
+                                        .checkSentenceLocal();
+                                    context
+                                        .read<CheckCompareWordLocalCubit>()
+                                        .checkCompareWordLocal();
+                                    context
+                                        .read<CheckListWordLocalCubit>()
+                                        .checkListWordBugisLocal();
+                                    context
+                                        .read<CheckListWordLocalCubit>()
+                                        .checkListWordIndoLocal();
+                                  }),
+                            ],
                           );
                         }
                       } else if (state is ListComparissonLoading) {
@@ -260,20 +116,6 @@ class _SplashPageState extends State<SplashPage> {
                         );
                       }
                     },
-                  ),
-                  const SizedBox(
-                    height: 42,
-                  ),
-                  Visibility(
-                    visible: true,
-                    child: PrimaryButton(
-                        title: "Ayo Mulai",
-                        onTap: () {
-                          Navigator.pushNamed(context, 'home');
-                        }),
-                  ),
-                  const SizedBox(
-                    height: 24,
                   ),
                 ],
               ),
